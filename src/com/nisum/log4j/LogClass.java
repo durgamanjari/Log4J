f416bf7bb2d30af9f81dcf22b33636ccb7376470
durgamanjari <dnarni@nisum.com>
2018-01-18 14:49:50 +0530

    First commit

---
+++ src/com/nisum/log4j/LogClass.java
@@ -0,0 +1,20 @@
+package com.nisum.log4j;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class LogClass {
+	private static Logger log=Logger.getLogger(LogClass.class);
+	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		log.setLevel(Level.TRACE);
+		
+		  log.trace("Trace Message!");
+	      log.debug("Debug Message!");
+	      log.info("Info Message!");
+	      log.warn("Warn Message!");
+	      log.error("Error Message!");
+	      log.fatal("Fatal Message!");
+	}
+}
